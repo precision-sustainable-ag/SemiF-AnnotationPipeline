a221d4fafcbdde33ee73fdbd3dbd28e0dbed3ab4
mkutu <mattkutugata@gmail.com>
2022-02-04 21:09:22 -0600

    simplify Metadata.py

--- Metadata.py
+++ Metadata.py
@@ -1,33 +1,31 @@
 import csv
 from dataclasses import dataclass, field
 
-
 @dataclass
-class Metadata:
-    
+class Metadata:    
     """ Data class to hold metadata """
 
     csv_path:       str
+    upload_id:      str    
+    time:           str    
+    location:       str    
+    cloud_cover:    str    
+    camera_height:  float  
+    camera_lens:    float  
+    pot_height:     float  
     name:           str     = "SemiField BenchBot Metadata"
-    upload_id:      str     = field(init=False, repr=True)
-    time:           str     = field(init=False, repr=True)
-    location:       str     = field(init=False, repr=True)
-    cloud_cover:    str     = field(init=False, repr=True)
-    camera_height:  float   = field(init=False, repr=True, metadata={"unit":"meters"})
-    camera_lens:    float   = field(init=False, repr=True,metadata={"unit":"millimeters"})
-    pot_height:     float   = field(init=False, repr=True, metadata={"unit":"meters"})
-    
-    def __post_init__(self):
-        csvdict = self.csv2dict()
-        self.time = csvdict["time"]
-        self.upload_id = csvdict["upload_id"]
-        self.location = csvdict["location"]
-        self.cloud_cover = csvdict["cloud_cover"]
-        self.camera_lens = float(csvdict["camera_lens"])
-        self.camera_height = float(csvdict["camera_height"])
-        self.pot_height = float(csvdict["pot_height"])
-        
-    
+
+    def __init__(self, csv_path):
+        self.csv_path       = csv_path
+        csvdict             = self.csv2dict()
+        self.upload_id      = csvdict["upload_id"]
+        self.time           = csvdict["time"]
+        self.location       = csvdict["location"]
+        self.cloud_cover    = csvdict["cloud_cover"]
+        self.camera_height  = float(csvdict["camera_height"])
+        self.camera_lens    = float(csvdict["camera_lens"])
+        self.pot_height     = float(csvdict["pot_height"])
+
     def csv2dict(self):
         with open(self.csv_path) as f:
             reader = csv.DictReader(f)
@@ -35,4 +33,4 @@ class Metadata:
             for row in reader:
                 for column, value in row.items():
                     result[column] = value
-        return result    
+        return result  
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 from Metadata import Metadata
+# meta = TestClass(0.2)
 meta = Metadata("data/sample_metadata.csv")
 print(meta)    
