4038032c7d86d979d55de0dc55396991e443e881
Matthew Kutugata <54868229+mkutu@users.noreply.github.com>
2022-04-29 21:46:36 -0500

    minor changes for config and df parsing

--- localize_plants.py
+++ localize_plants.py
@@ -21,7 +21,7 @@ def load_model(model_path):
     model.agnostic = False  # NMS class-agnostic
     model.multi_label = False  # NMS multiple labels per box
     # model.classes = None  # (optional list) filter by class, i.e. = [0, 15, 16]
-    model.max_det = 1000  # maximum number of detections per image
+    model.max_det = 1000  # maximum number of detections per imag
     model.amp = False  # Automatic Mixed Precision (AMP) inference
     # model.cpu()  # CPU
     return model
@@ -33,10 +33,6 @@ def inference(imgpath, model, save_detection=False):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     # Get results
     results = model(img, size=640)
-    results.save()
-    if save_detection:
-        crops = results.crop()
-
     # Convert to pd dataframe
     df = results.pandas().xyxy[0]
     # Add imgfilename to columns
@@ -44,21 +40,23 @@ def inference(imgpath, model, save_detection=False):
         df.at[i, 'imgname'] = imgpath.name
     # return df, imgpath, crops
     if save_detection:
-        return df, imgpath, crops
+        return df, img, imgpath
     else:
         return df, imgpath
 
 
 def main(cfg: DictConfig) -> None:
-    save_detection = cfg.general.save_detection
+    save_detection = cfg.detect.save_detection
     ## Define directories
-    model_path = cfg.general.model_path
+    model_path = cfg.detect.model_path
     imagedir = Path(cfg.general.imagedir)
-    csv_savepath = Path(cfg.general.imagedir, "detections.csv")
+    batchdir = Path(cfg.general.batchdir)
+    csv_savepath = Path(cfg.general.batchdir, "detections.csv")
 
-    # Crop savepath
-    cropsave_dir = Path(imagedir, "detection_crops")
-    cropsave_dir.mkdir(parents=True, exist_ok=True)
+    if save_detection:
+        # Crop savepath
+        cropsave_dir = Path(batchdir, "detection_crops")
+        cropsave_dir.mkdir(parents=True, exist_ok=True)
 
     ## Get image files
     images = sorted(imagedir.rglob("*.jpg"), reverse=True)
@@ -68,19 +66,24 @@ def main(cfg: DictConfig) -> None:
     dfimgs = []
     for idx, imgp in tqdm(enumerate(images),
                           desc="Localizing Plants",
+                          colour="#9266c4",
                           total=len(images)):
         if not save_detection:
             df, imgpath = inference(imgp, model, save_detection=save_detection)
 
         if save_detection:
-            df, imgpath, crops = inference(imgp,
-                                           model,
-                                           save_detection=save_detection)
+            df, img, imgpath = inference(imgp,
+                                         model,
+                                         save_detection=save_detection)
             num = 0
-            for crop in crops:
+            for row in df.itertuples():
+                x1, y1 = int(row.xmin), int(row.ymin)
+                x2, y2 = int(row.xmax), int(row.ymax)
+                crop = img[y1:y2, x1:x2]
                 fname = Path(imgpath).stem + "_" + str(num) + ".jpg"
                 cropsave_path = Path(cropsave_dir, fname)
-                cv2.imwrite(cropsave_path, crop["im"])
+                cv2.imwrite(cropsave_path,
+                            cv2.cvtColor(crop, cv2.COLOR_BGR2RGB))
                 num += 1
         dfimgs.append(df)
 
