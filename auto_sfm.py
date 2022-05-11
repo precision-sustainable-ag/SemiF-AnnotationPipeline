a4be4045c1bd79cbc20f3a9ee3ad52fcda78fbbd
Chinmay Savadikar <savadikarc@gmail.com>
2022-05-11 19:12:41 -0400

    calling autosfm from SEMIF

--- auto_sfm.py
+++ auto_sfm.py
@@ -1,4 +1,7 @@
 import shutil
+import subprocess
+from pathlib import Path
+
 from omegaconf import DictConfig, OmegaConf
 import yaml
 
@@ -11,8 +14,24 @@ def main(cfg: DictConfig) -> None:
     with open(save_file, "w") as f:
         yaml.dump(autosfm_config, f)
 
-    # TODO: subprocess to run autoSfM docker container
+    # Compose the command
+    exe_command = f"docker run \
+    -v {cfg.autosfm.autosfm_volume}:/home/psi_docker/autoSfM/volumes \
+    -v {cfg.autosfm.autosfm_storage}:/home/psi_docker/autoSfM/storage \
+    -v {cfg.autosfm.autosfm_exports}:/home/psi_docker/autoSfM/exports \
+    sfm {cfg.autosfm.metashape_key}"
+
+    try:
+        # Run the autoSfM command
+        subprocess.run(exe_command, shell=True, check=True)    
+    except Exception as e:
+        raise e
 
     # Make a copy of the autoSfM config for documentation
     copy_file = cfg.autosfm.config_copy_path
     shutil.copy(save_file, copy_file)
+
+    # Copy the exports to the desired location
+    export_src = Path(cfg.autosfm.autosfm_exports, cfg.general.batch_id)
+    export_dst = Path(cfg.general.batchdir, "autosfm")
+    shutil.move(export_src, export_dst)
