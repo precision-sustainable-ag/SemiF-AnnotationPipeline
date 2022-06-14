6d5e786949e6be07e76b04bffaf65141533df8ab
Matthew Kutugata <54868229+mkutu@users.noreply.github.com>
2022-06-13 22:35:03 -0500

    mostly auto code formatter except get_species_info

--- assign_species.py
+++ assign_species.py
@@ -1,27 +1,34 @@
 import json
 from pathlib import Path
-from omegaconf import DictConfig, OmegaConf
-from dacite import Config, from_dict
-import shapefile
+
 import matplotlib.path as mplPath
 import numpy as np
+import shapefile
+from dacite import Config, from_dict
+from omegaconf import DictConfig, OmegaConf
 from tqdm import tqdm
+
 from semif_utils.datasets import ImageData
+from semif_utils.segment_utils import get_species_info
+
 
 def inpolygon(point, polygon):
     bb_path = mplPath.Path(np.array(polygon))
     return bb_path.contains_point(point)
 
+
 def main(cfg: DictConfig) -> None:
 
     batch_id = cfg.general.batch_id
+    species_info = cfg.data.species
     location = batch_id.split("_")[0]
-    shapefile_path = Path(cfg.data.utilsdir, location, "shapefiles", f"{location}.shp")
+    shapefile_path = Path(cfg.data.utilsdir, location, "shapefiles",
+                          f"{location}.shp")
 
     # Read the shapefiles
     sf = shapefile.Reader(shapefile_path)
     polygons = sf.shapeRecords()
-    
+
     metadata_path = Path(cfg.data.batchdir, "metadata")
     image_metadata_files = metadata_path.glob("*.json")
 
@@ -34,12 +41,14 @@ def main(cfg: DictConfig) -> None:
         for bbox in imgdata.bboxes:
             # Assign species
             for polygon in polygons:
-                
+
                 # polygon_bbox is structured as [x_lower,y_lower,x_upper,y_upper]
                 polygon_bbox = polygon.shape.bbox
                 # Find if significant overlap
-                horizontal = polygon_bbox[0] < bbox.global_centroid[0] and polygon_bbox[2] > bbox.global_centroid[0]
-                vertical = polygon_bbox[1] < bbox.global_centroid[1] and polygon_bbox[3] > bbox.global_centroid[1]
+                horizontal = polygon_bbox[0] < bbox.global_centroid[
+                    0] and polygon_bbox[2] > bbox.global_centroid[0]
+                vertical = polygon_bbox[1] < bbox.global_centroid[
+                    1] and polygon_bbox[3] > bbox.global_centroid[1]
 
                 in_polygon_bbox = horizontal and vertical
 
@@ -48,16 +57,21 @@ def main(cfg: DictConfig) -> None:
                     # Fix handling polygon with inner parts
                     parts_in_polygon = polygon.shape.parts
 
-                    if len(parts_in_polygon)==1:
+                    if len(parts_in_polygon) == 1:
                         polygon_points = polygon.shape.points
                     else:
-                        polygon_points = polygon.shape.points[0:parts_in_polygon[1]]
+                        polygon_points = polygon.shape.points[
+                            0:parts_in_polygon[1]]
 
                     x, y = bbox.global_centroid[0], bbox.global_centroid[1]
-                    in_polygon = inpolygon((x,y),polygon_points)
+                    in_polygon = inpolygon((x, y), polygon_points)
 
                     if in_polygon:
-                        bbox.assign_species(polygon.record["species"])
+                        spec_info = get_species_info(species_info,
+                                                     polygon.record["species"])
+                        bbox.assign_species(spec_info)
+                        # get_species_info(species_info)
+
                         break
         # Save
         imgdata.save_config(metadata_path)
