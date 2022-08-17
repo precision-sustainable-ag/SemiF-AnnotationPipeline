8dad24171e00e21d56cd5b79c5e67c94406f7c82
Chinmay Savadikar <savadikarc@gmail.com>
2022-08-17 00:03:38 -0400

    copying masks to autosfm storage

--- auto_sfm.py
+++ auto_sfm.py
@@ -23,6 +23,13 @@ def main(cfg: DictConfig) -> None:
     autosfm_storage = Path(cfg.autosfm.autosfm_storage, cfg.general.batch_id)
     images_dst = Path(autosfm_storage, "developed")
     shutil.copytree(images_src, images_dst)
+
+    # Copy the masks
+    images_src = Path(cfg.data.batchdir, "masks")
+    autosfm_storage = Path(cfg.autosfm.autosfm_storage, cfg.general.batch_id)
+    images_dst = Path(autosfm_storage, "masks")
+    shutil.copytree(images_src, images_dst)
+
     # Get GCP file based on batch ID
     state_id = cfg.general.batch_id.split("_")[0]
     gcp_src = [x for x in Path(cfg.data.utilsdir, state_id).glob("*.csv")][0]
