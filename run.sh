b8e87dc75dbba7b22a02a4f9c2a8cfdc0ba06aa1
weedsci <mattkutugata@gmail.com>
2022-08-31 10:40:13 -0500

    moved sh files to scripts and segment_species to utils

--- run.sh
+++
@@ -1,5 +0,0 @@
-export agisoft_LICENSE="/home/weedsci/matt/SemiF-AnnotationPipeline/autosfm/volumes/metashape/metashape-pro/metashape.lic"
-python SEMIF.py \
-general.batch_id=MD_2022-08-24 \
-autosfm.metashape_key=asd \
-general.task=segment_vegetation
