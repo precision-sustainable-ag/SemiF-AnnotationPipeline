3e5dfcf99d521913061aec5d9addf383fe03ab24
mkutu <mattkutugata@gmail.com>
2023-10-23 10:32:48 -0400

    minor move scripts

--- move_data/scripts/execute_move.sh
+++ move_data/scripts/execute_move.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-bash copy_batch_to_localterm.sh ../../.batchlogs/upload_batches.txt
-bash copy_cutouts_to_localterm.sh ../../.batchlogs/upload_batches.txt
+bash copy_batch_to_longterm.sh ../../.batchlogs/upload_batches.txt
+bash copy_cutouts_to_longterm.sh ../../.batchlogs/upload_batches.txt
 bash upload_batch_from_list.sh ../../.batchlogs/upload_batches.txt
 bash upload_cutout_from_list.sh ../../.batchlogs/upload_batches.txt
