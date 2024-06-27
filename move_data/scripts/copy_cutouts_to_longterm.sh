4984428612c34ebcf0e453a4b57450b1bd3cc003
mkutu <mattkutugata@gmail.com>
2024-06-27 13:35:48 -0400

    log movedata activity

--- move_data/scripts/copy_cutouts_to_longterm.sh
+++ move_data/scripts/copy_cutouts_to_longterm.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-# Copies local cutouts and associated data to longterm storage.
+LOGFILE="./pipeline.log"
 
+# Copies local cutouts and associated data to longterm storage.
 
 BATCHES=$1 # txt file of cutout batches that you want to upload to semifield-cutouts
 
@@ -10,22 +11,19 @@ DST="/mnt/research-projects/s/screberg/GROW_DATA/semifield-cutouts"
 
 for line in `cat $BATCHES`; do
     echo
-    echo "Copying cutouts $line to longterm storage"
+    echo "Copying cutouts $line to longterm storage" >> $LOGFILE
+    echo "Copying to destination: $DST" >> $LOGFILE
 
-    
     SRC=$SRCPARENT/$line
     CSVFILE="$SRCPARENT/$line/$line.csv"
 
     if [ ! -f "$CSVFILE" ]; then
-        echo "CSV for $line not present. Exiting"
+        echo "CSV for $line not present. Exiting" >> $LOGFILE
         exit 1
     fi
     
     cp -r $SRC $DST
-    echo "Done copying cutouts $line to longterm storage"
+    echo "Done copying cutouts $line to longterm storage: $DST" >> $LOGFILE
     echo
     
 done
-
-
-
--- move_data/scripts/execute_move.sh
+++ move_data/scripts/execute_move.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-bash copy_batch_to_longterm.sh ../../.batchlogs/upload_batches.txt
-bash copy_cutouts_to_longterm.sh ../../.batchlogs/upload_batches.txt
-bash upload_batch_from_list.sh ../../.batchlogs/upload_batches.txt
-bash upload_cutout_from_list.sh ../../.batchlogs/upload_batches.txt
+LOGFILE="./pipeline.log"
+
+echo "Starting pipeline at $(date)" >> $LOGFILE
+
+bash copy_batch_to_longterm.sh ../../.batchlogs/upload_batches.txt >> $LOGFILE 2>&1
+bash copy_cutouts_to_longterm.sh ../../.batchlogs/upload_batches.txt >> $LOGFILE 2>&1
+bash upload_batch_from_list.sh ../../.batchlogs/upload_batches.txt >> $LOGFILE 2>&1
+bash upload_cutout_from_list.sh ../../.batchlogs/upload_batches.txt >> $LOGFILE 2>&1
+
+echo "Pipeline finished at $(date)" >> $LOGFILE
