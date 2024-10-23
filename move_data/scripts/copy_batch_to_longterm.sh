64a038b2cab1394f0429f3a77321a9a1cf3d1295
Matthew Kutugata <mattkutugata@gmail.com>
2024-10-23 11:27:59 -0400

    added third storage location for lockers

--- move_data/scripts/copy_batch_to_longterm.sh
+++ move_data/scripts/copy_batch_to_longterm.sh
@@ -6,42 +6,67 @@ LOGFILE="./pipeline.log"
 
 BATCHES=$1 # txt file of batches that you want to upload to cutouts
 
-DST="/mnt/research-projects/s/screberg/longterm_images/semifield-developed-images"
-# DST="/mnt/research-projects/s/screberg/GROW_DATA/semifield-developed-images"
+PRIMARY_DST="/mnt/research-projects/s/screberg/longterm_images/semifield-developed-images"
+SECONDARY_DST="/mnt/research-projects/s/screberg/GROW_DATA/semifield-developed-images"
+TERTIARY_DST="/mnt/research-projects/s/screberg/longterm_images2/semifield-developed-images"
 SRCPARENT="/home/psa_images/SemiF-AnnotationPipeline/data/semifield-developed-images"
 
-for line in `cat $BATCHES`; do
-    echo
+for line in $(cat $BATCHES); do
     echo "Starting batch $line to longterm storage" >> $LOGFILE
 
-    DST_BATCHDIR=$DST/$line
     SRC=$SRCPARENT/$line
-    echo "Copying to destination: $DST_BATCHDIR" >> $LOGFILE
+    DST_BATCHDIR_PRIMARY=$PRIMARY_DST/$line
+    DST_BATCHDIR_SECONDARY=$SECONDARY_DST/$line
+    DST_BATCHDIR_TERTIARY=$TERTIARY_DST/$line
+
+    # Determine destination based on which one already contains images or none
+    if [ -d "$DST_BATCHDIR_PRIMARY/images" ]; then
+        DST_BATCHDIR=$DST_BATCHDIR_PRIMARY
+        echo "Primary destination already contains images. Skipping image copy to primary." >> $LOGFILE
+    elif [ -d "$DST_BATCHDIR_SECONDARY/images" ]; then
+        DST_BATCHDIR=$DST_BATCHDIR_SECONDARY
+        echo "Secondary destination already contains images. Skipping image copy to secondary." >> $LOGFILE
+    
+    elif [ -d "$DST_BATCHDIR_TERTIARY/images" ]; then
+        DST_BATCHDIR=$DST_BATCHDIR_TERTIARY
+        echo "Tertiary destination already contains images. Skipping image copy to tertiary." >> $LOGFILE
+    else
+        # If no images in primary or secondary, copy to primary
+        DST_BATCHDIR=$DST_BATCHDIR_PRIMARY
+        echo "Neither primary nor secondary destination contains images. Setting destination to primary." >> $LOGFILE
+    fi
+
+    # Create destination directory if it does not exist
     if [ ! -d "$DST_BATCHDIR" ]; then
-        echo "Making destination directory" >> $LOGFILE
-        mkdir $DST_BATCHDIR
+        echo "Making destination directory $DST_BATCHDIR" >> $LOGFILE
+        mkdir -p $DST_BATCHDIR
     fi
     
+    # Define paths for each subfolder
     DSTimages="$DST_BATCHDIR/images"
     images="$SRC/images"
     asfm="$SRC/autosfm/reference"
-	meta_masks="$SRC/meta_masks"
-	metadata="$SRC/metadata"
+    meta_masks="$SRC/meta_masks"
+    metadata="$SRC/metadata"
 
+    # Copy images only if not already present in destination
     if [ -d "$DSTimages" ]; then
-        echo "Image directory exists. Skipping image copy." >> $LOGFILE
+        echo "Image directory exists in $DST_BATCHDIR. Skipping image copy." >> $LOGFILE
     else
-        echo "Copying images because they do not exist in longterm storage" >> $LOGFILE
+        echo "Copying images to $DSTimages" >> $LOGFILE
         cp -r $images $DSTimages
     fi
 
-    echo "Copying asfm reference data" >> $LOGFILE
+    # Copy additional directories regardless
+    echo "Copying asfm reference data to $DST_BATCHDIR" >> $LOGFILE
     cp -r $asfm $DST_BATCHDIR
-    echo "Copying meta_masks" >> $LOGFILE
+
+    echo "Copying meta_masks to $DST_BATCHDIR" >> $LOGFILE
     cp -r $meta_masks $DST_BATCHDIR
-    echo "Copying metadata" >> $LOGFILE
+
+    echo "Copying metadata to $DST_BATCHDIR" >> $LOGFILE
     cp -r $metadata $DST_BATCHDIR
+
     echo "Done copying to longterm storage for batch $line" >> $LOGFILE
     echo
-
 done
--- move_data/scripts/copy_cutouts_to_longterm.sh
+++ move_data/scripts/copy_cutouts_to_longterm.sh
@@ -6,26 +6,53 @@ LOGFILE="./pipeline.log"
 
 BATCHES=$1 # txt file of cutout batches that you want to upload to semifield-cutouts
 
+# Paths for developed images from the previous script
+PRIMARY_DEV_IMAGES="/mnt/research-projects/s/screberg/longterm_images/semifield-developed-images"
+SECONDARY_DEV_IMAGES="/mnt/research-projects/s/screberg/GROW_DATA/semifield-developed-images"
+TERTIARY_DEV_IMAGES="/mnt/research-projects/s/screberg/longterm_images2/semifield-developed-images"
 
-DST="/mnt/research-projects/s/screberg/longterm_images/semifield-cutouts/"
-# DST="/mnt/research-projects/s/screberg/GROW_DATA/semifield-cutouts"
+# Paths for cutouts
+PRIMARY_DST="/mnt/research-projects/s/screberg/longterm_images/semifield-cutouts"
+SECONDARY_DST="/mnt/research-projects/s/screberg/GROW_DATA/semifield-cutouts"
 SRCPARENT="/home/psa_images/SemiF-AnnotationPipeline/data/semifield-cutouts"
 
-for line in `cat $BATCHES`; do
-    echo
-    echo "Copying cutouts $line to longterm storage" >> $LOGFILE
-    echo "Copying to destination: $DST" >> $LOGFILE
+for line in $(cat $BATCHES); do
+    echo "Processing cutouts $line for longterm storage" >> $LOGFILE
 
     SRC=$SRCPARENT/$line
-    CSVFILE="$SRCPARENT/$line/$line.csv"
+    CSVFILE="$SRC/$line.csv"
 
+    # Check if the CSV file exists for the batch
     if [ ! -f "$CSVFILE" ]; then
-        echo "CSV for $line not present. Exiting" >> $LOGFILE
-        exit 1
+        echo "CSV for $line not present. Skipping batch $line." >> $LOGFILE
+        continue
     fi
     
-    cp -r $SRC $DST
-    echo "Done copying cutouts $line to longterm storage: $DST" >> $LOGFILE
-    echo
+    # Determine the destination based on where "meta_masks" is present in the developed images
+    DEV_IMAGES_BATCH_PRIMARY=$PRIMARY_DEV_IMAGES/$line/meta_masks
+    DEV_IMAGES_BATCH_SECONDARY=$SECONDARY_DEV_IMAGES/$line/meta_masks
+    DEV_IMAGES_BATCH_TERTIARY=$TERTIARY_DEV_IMAGES/$line/meta_masks
+
+    if [ -d "$DEV_IMAGES_BATCH_PRIMARY" ]; then
+        DST_BATCHDIR=$PRIMARY_DST
+        echo "Primary destination contains developed images for batch $line. Copying cutouts to primary." >> $LOGFILE
+    elif [ -d "$DEV_IMAGES_BATCH_SECONDARY" ]; then
+        DST_BATCHDIR=$SECONDARY_DST
+        echo "Secondary destination contains developed images for batch $line. Copying cutouts to secondary." >> $LOGFILE
     
+    elif [ -d "$DEV_IMAGES_BATCH_TERTIARY" ]; then
+        DST_BATCHDIR=$DEV_IMAGES_BATCH_TERTIARY
+        echo "Tertiary destination contains developed images for batch $line. Copying cutouts to tertiary." >> $LOGFILE
+    else
+        echo "Neither primary nor secondary nor tertiary destination contains developed images for batch $line. Skipping." >> $LOGFILE
+        continue
+    fi
+
+    # Copy the cutout data to the determined destination
+    echo "Copying cutouts for batch $line to $DST_BATCHDIR" >> $LOGFILE
+    cp -r $SRC $DST_BATCHDIR
+
+    echo "Done copying cutouts $line to longterm storage: $DST_BATCHDIR" >> $LOGFILE
+    echo
+
 done
